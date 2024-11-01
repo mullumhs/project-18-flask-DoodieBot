839d4e9d39ba4c8b7021a4fa66ee6b3de1287d43
DoodieBot <140124349+DoodieBot@users.noreply.github.com>
2024-11-01 11:00:35 +1100

    made half functioning web page

--- task2.py
+++ task2.py
@@ -2,10 +2,20 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/basic')
-def basic():
+@app.route('/inventory')
+def inventory():
+    inventory_items = ['apple', 'banana', 'cherry']
+    return render_template('items.html', inventory=inventory_items)
 
-    return render_template('basic.html')
+@app.route('/search')
+def search():
+    query = request.args.get('q', '')
+    category = request.args.get('category', 'all')
+    return f'Searching for "{query}" in category: {category}'
+
+@app.route('/')
+def page():
+    return render_template('base.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
